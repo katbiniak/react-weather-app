5893a21193819fbd639240578c1e2a0de5b72ad4
katbiniak <katelynbiniak@gmail.com>
2023-02-21 10:17:33 -0500

    Adding new font mixin and updating css to use it, fixing wind speed to display even if it is set to 0 mph

--- src/js/util/windSpeed.js
+++ src/js/util/windSpeed.js
@@ -8,13 +8,11 @@ import { UNITS } from "../constants/units";
 * @param {String} currentUnit: current selected units, either M or I see UNITS constants file
 */
 export function getWindSpeed(windSpeed, currentUnit) {
-    if (windSpeed) {
-    var speed = windSpeed;
-        // metrics enpoint returns m/s instead of mph so multiple to get correct value
-        if (currentUnit === UNITS.CELSIUS) {
-            speed = speed * 2.237;
-        }
-        return `${Math.round(speed)} ${LABELS.MPH}`;
+    var speed = windSpeed || 0;
+    
+    // metrics enpoint returns m/s instead of mph so multiple to get correct value
+    if (currentUnit === UNITS.CELSIUS) {
+        speed = speed * 2.237;
     }
-    return ''; 
+    return `${Math.round(speed)} ${LABELS.MPH}`;
 }
